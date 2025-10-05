d0cf06b0f9753c9094753404eef5f686226f6765
sabarish <sabarish.science@gmail.com>
2025-10-05 21:45:25 +0200

    remove

--- debug_vector.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash
-cmake -B build_vector -DMODE=DEBUG_VECTOR
-cmake --build build_vector
-echo
-echo
-echo "################################################################################"
-echo "Errors will start appearing from here on if found :"
-echo "##########"
-./build_vector/vec_debug.out
